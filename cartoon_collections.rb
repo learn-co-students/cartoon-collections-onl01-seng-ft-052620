73093db59c24124b5f3bc0d30a93f4bd16cc9600
Barrett Anderson <barrettanderson1989@gmail.com>
2020-05-29 23:08:01 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+
+def roll_call_dwarves(dwarves)
+    #binding.pry
+  dwarves.each_with_index do |name, index|
+    puts "#{index +1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(veggies)
+  veggies.collect! {|name| name.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls(calls_long)
+  index = 0
+  if calls_long.any? {|index| index.length > 4}
+    return true
+  else
+    return false
+  index += 1
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+
+def find_the_cheese(cheddar_cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  cheddar_cheese.find{|i|cheese_types.include?(i)} #true
+
 end
